4e0fb3a4eb678d125c001895b9db7c41c0551e29
Ismael Osman <muhammadismaelosman@gmail.com>
2024-03-26 08:50:14 +0800

    tried out logging with python's inbuilt module

---
+++ main.py
@@ -0,0 +1,17 @@
+import logging
+import datetime
+from time import sleep
+import os
+
+if os.path.isfile('./test.log'):
+    logging.basicConfig(filename=f'./test.log', filemode='a', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+    while True:
+        current_date_time = datetime.datetime.now()
+        formatted_date_time = current_date_time.strftime('%Y-%m-%d-%H%M%S')
+        logging.info(f'{formatted_date_time} - This is a log message')
+        sleep(5)
+
+
+
+
+ 
